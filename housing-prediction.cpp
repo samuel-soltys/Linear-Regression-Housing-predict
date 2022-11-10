dbdee31ea5e05694960cec244ad0cad388772f95
Samuel Šoltys <samuelsasanka@gmail.com>
2022-11-10 10:39:26 +0100

    Cost function implementation

--- housing-prediction.cpp
+++ housing-prediction.cpp
@@ -6,6 +6,23 @@ using namespace std;
 
 typedef long long ll;
 
+float linear_function(int x, float w, float b) {
+    return(w * x + b);
+}
+
+float squared_error_cost_function(float w, float b, vector<pair<int,int> > training_houses) {
+    float J = 0;
+    
+    // sum((y_(i) - y(i))^2)
+    for(int i = 0; i < training_houses.size(); i++) {
+        J += pow(linear_function(training_houses[i].first, w, b) - training_houses[i].second, 2);
+    }
+    
+    // J = 1/2m * sum((y(i) - y(i))^2)    
+    return(J / (2 * training_houses.size()));
+}
+
+
 int main() {
     
     vector<pair<int,int> > training_houses;
@@ -20,6 +37,12 @@ int main() {
     training_houses.push_back(make_pair(92, 399990));
     training_houses.push_back(make_pair(500, 870000));
     
+    // y = linear_function(x)
+    float w = 0, b = 0;
     
+    // test of cost function
+    float cost_function = squared_error_cost_function(w, b, training_houses);
+    
+    cout << "Squared_error_cost function value: " << cost_function << endl;
     return 0;
 }
