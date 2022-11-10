84016da1295578a7f5a8ee83c46efb193eb33d32
Samuel Šoltys <samuelsasanka@gmail.com>
2022-11-10 10:32:26 +0100

    Housing prediction data prepare

---
+++ housing-prediction.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<cmath>
+#include<vector>
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    
+    vector<pair<int,int> > training_houses;
+    
+    training_houses.push_back(make_pair(94, 660000));
+    training_houses.push_back(make_pair(211, 890000));
+    training_houses.push_back(make_pair(154, 648000));
+    training_houses.push_back(make_pair(150, 535000));
+    training_houses.push_back(make_pair(226, 300000));
+    training_houses.push_back(make_pair(150, 569000));
+    training_houses.push_back(make_pair(120, 780000));
+    training_houses.push_back(make_pair(92, 399990));
+    training_houses.push_back(make_pair(500, 870000));
+    
+    
+    return 0;
+}
